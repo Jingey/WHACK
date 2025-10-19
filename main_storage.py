6e799f2ddfc9754afc94e8c00f57588bb32b28c9
Jingey <118936838+Jingey@users.noreply.github.com>
2025-10-19 08:49:22 +0100

    Added LLM memory <3

--- main_storage.py
+++ main_storage.py
@@ -1,3 +1,5 @@
+from langchain_ollama import OllamaLLM
+
 from wires import Wire, Bus
 from logs import log
 
@@ -12,7 +14,7 @@ class MainStorage:
         self.address_bus = address_bus
         self.data_bus = data_bus
         self.rw_bus = rw_bus
-        self.mem = [0] * (2**16)
+        self.mem = MainStorageLLM()
         self.enable.enlist(self.execute)
 
     def execute(self):
@@ -23,7 +25,7 @@ class MainStorage:
 
     def read(self):
         addr = self.address_bus.read_data()
-        log.log_ms_load(addr, self.mem[addr])
+        # log.log_ms_load(addr, self.mem[addr])
         self.data_bus.set_data(self.mem[addr])
 
     def write(self):
@@ -33,4 +35,34 @@ class MainStorage:
         self.mem[addr] = data
 
     def load(self, data):
-        self.mem = data + [0] * abs((2**16) - len(data))
+        for i, v in enumerate(data):
+            self.mem[i] = v
+
+
+class MainStorageLLM:
+    def __init__(self):
+        self.mem = []
+        self.llm = OllamaLLM(model="wizardlm2", temperature=0, reasoning=False)
+
+    def __setitem__(self, key, value):
+        block = key // 4
+        offset = key % 4
+        if block + 1 > len(self.mem):
+            for i in range(block + 1 - len(self.mem)):
+                self.mem.append([0] * 4)
+        self.mem[block][offset] = value
+
+    def __getitem__(self, item):
+        result = 0
+
+        block = item // 4
+        offset = item % 4
+
+        prompt = f"""Given the array A = {self.mem[block]} and the index {offset}, return only the integer at that index in the array.
+        Important: Output only the integer value at that index. Do not include any explanation, text, or punctuation. Just output the integer."""
+
+        while result != self.mem[block][offset]:
+            prompt += f" The answer is not: {result}."
+            result = int(self.llm.invoke(prompt))
+
+        return result
